30b368450e7b5c6dabb3df4fc2360341bebdc24a
Melissa Freitas <melissa.mf29@gmail.com>
2022-06-21 23:09:24 -0300

    Base do programa

---
+++ Contatos_MVC/Controllers/ContatoController.cs
@@ -0,0 +1,20 @@
+﻿using Contatos_MVC.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Contatos_MVC.Controllers
+{
+    public class ContatoController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+      
+    }
+}
